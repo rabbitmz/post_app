9242e8bd92b0f9bd00232f3af0e43c0b04762da8
Yuri Wingester <yuri2jwingester@gmail.com>
2019-02-18 16:24:16 +0200

    adding mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
@@ -9,7 +13,16 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use("/api/posts",(req, res, next) => {
+app.post("/api/posts", (req,res, next) => {
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({
+    message: 'Post added successfuly'
+  });
+});
+
+
+app.get("/api/posts",(req, res, next) => {
 
   const posts = [
     {id: "123ADS", title: "first post", content: "my first post from the server"},
--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -24,13 +24,17 @@ export class PostService {
     });
   }
 
-  addPost(title: string, content: string) {
-       const post: Post = {id: null, title: title, content: content};
+  addPost(title: string, content: string) {    
+    const post: Post = {id: null, title: title, content: content};
+    this.http.post<{message: string}>('http://localhost:3000/api/posts', post)
+    .subscribe(responseData => {
+      console.log(responseData.message);
+      this.posts.push(post);
+      this.postUpdated.next([...this.posts]);
+    });
 
     this.posts.push(post);
-
     this.postUpdated.next([...this.posts]);
-
   }
 
 }
